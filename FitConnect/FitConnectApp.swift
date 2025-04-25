00948c2e8ff775ae3d86d55cbcf88e512ed99624
Can Acar <canacar192@icloud.com>
2025-04-25 22:29:21 +0300

    Initial Commit

---
+++ FitConnect/FitConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitConnectApp.swift
+//  FitConnect
+//
+//  Created by Can Acar on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FitConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
